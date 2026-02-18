4fe61faee0a9acaeb025466b28334c0b532a9286
architkiran <architkiran@gmail.com>
2026-02-17 19:47:14 -0500

    break: breaking again

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def add(a, b):
 
 @app.route("/")
 def hello():
-  return jsonify(message="Hello from CI/CD Lab!")
+  return jsonify(message="Pipeline break!")
 
 @app.route("/add")
 def add_endpoint():
